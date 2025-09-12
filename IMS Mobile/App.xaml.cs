d480dc673ad8515d460c2578e97b2751428ea04c
Bhbored <bhbored2022@gmail.com>
2025-09-12 13:51:35 +0300

    signe the app

--- IMS Mobile/App.xaml.cs
+++ IMS Mobile/App.xaml.cs
@@ -43,7 +43,7 @@ namespace IMS_Mobile
         public App(Supabase.Client supabaseClient, SyncService syncService, SupabaseAuthService _authservice, IServiceProvider serviceProvider)
         {
             InitializeComponent();
-            SyncfusionLicenseProvider.RegisterLicense("Ngo9BigBOggjHTQxAR8/V1JEaF5cXmRCf1FpRmJGdld5fUVHYVZUTXxaS00DNHVRdkdmWXlceHRTQ2ZYWUN/XkFWYEk=");
+            SyncfusionLicenseProvider.RegisterLicense("Ngo9BigBOggjHTQxAR8/V1JFaF5cXGRCf1FpRmJGdld5fUVHYVZUTXxaS00DNHVRdkdmWXZfcnRTRGBYUUN/V0ZWYEg=");
             _supabaseClient = supabaseClient;
             _syncService = syncService;
             AuthService = _authservice;
