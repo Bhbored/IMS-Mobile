596742acdedfa5ce5ece5a6209eabd7d44f31c8e
Bhbored <bhbored2022@gmail.com>
2025-09-13 14:57:35 +0300

    updated icon and splash

--- IMS Mobile/Converters/GlobalCurrencyConverter.cs
+++ IMS Mobile/Converters/GlobalCurrencyConverter.cs
@@ -1,4 +1,4 @@
-using System.Globalization;
+﻿using System.Globalization;
 using IMS_Mobile.MVVM.Models;
 using IMS_Mobile.DB;
 
@@ -27,7 +27,7 @@ namespace IMS_Mobile.Converters
             }
             catch
             {
-                _currentCurrency = Currency.USD; // Default fallback
+                _currentCurrency = Currency.USD; 
             }
         }
 
@@ -40,106 +40,106 @@ namespace IMS_Mobile.Converters
         {
             return _currentCurrency switch
             {
-                Currency.USD => "USD",
-                Currency.EUR => "EUR",
-                Currency.GBP => "GBP",
-                Currency.JPY => "JPY",
-                Currency.LBP => "LBP",
-                Currency.CAD => "CAD",
-                Currency.AUD => "AUD",
-                Currency.CHF => "CHF",
-                Currency.CNY => "CNY",
-                Currency.SEK => "SEK",
-                Currency.NOK => "NOK",
-                Currency.DKK => "DKK",
-                Currency.PLN => "PLN",
-                Currency.CZK => "CZK",
-                Currency.HUF => "HUF",
-                Currency.RON => "RON",
-                Currency.BGN => "BGN",
-                Currency.HRK => "HRK",
-                Currency.RSD => "RSD",
-                Currency.MKD => "MKD",
-                Currency.BAM => "BAM",
-                Currency.ALL => "ALL",
-                Currency.MNT => "MNT",
-                Currency.KRW => "KRW",
-                Currency.SGD => "SGD",
-                Currency.HKD => "HKD",
-                Currency.TWD => "TWD",
-                Currency.THB => "THB",
-                Currency.MYR => "MYR",
-                Currency.IDR => "IDR",
-                Currency.PHP => "PHP",
-                Currency.VND => "VND",
-                Currency.INR => "INR",
-                Currency.PKR => "PKR",
-                Currency.BDT => "BDT",
-                Currency.LKR => "LKR",
-                Currency.NPR => "NPR",
-                Currency.AFN => "AFN",
-                Currency.IRR => "IRR",
-                Currency.IQD => "IQD",
-                Currency.JOD => "JOD",
-                Currency.KWD => "KWD",
-                Currency.BHD => "BHD",
-                Currency.QAR => "QAR",
-                Currency.AED => "AED",
-                Currency.OMR => "OMR",
-                Currency.SAR => "SAR",
-                Currency.YER => "YER",
-                Currency.EGP => "EGP",
-                Currency.MAD => "MAD",
-                Currency.TND => "TND",
-                Currency.DZD => "DZD",
-                Currency.LYD => "LYD",
-                Currency.SDG => "SDG",
-                Currency.ETB => "ETB",
-                Currency.KES => "KES",
-                Currency.UGX => "UGX",
-                Currency.TZS => "TZS",
-                Currency.ZAR => "ZAR",
-                Currency.NGN => "NGN",
-                Currency.GHS => "GHS",
-                Currency.XOF => "XOF",
-                Currency.XAF => "XAF",
-                Currency.BRL => "BRL",
-                Currency.ARS => "ARS",
-                Currency.CLP => "CLP",
-                Currency.COP => "COP",
-                Currency.MXN => "MXN",
-                Currency.PEN => "PEN",
-                Currency.UYU => "UYU",
-                Currency.VES => "VES",
-                Currency.RUB => "RUB",
-                Currency.UAH => "UAH",
-                Currency.BYN => "BYN",
-                Currency.KZT => "KZT",
-                Currency.UZS => "UZS",
-                Currency.KGS => "KGS",
-                Currency.TJS => "TJS",
-                Currency.TMT => "TMT",
-                Currency.AZN => "AZN",
-                Currency.AMD => "AMD",
-                Currency.GEL => "GEL",
-                Currency.TRY => "TRY",
-                Currency.JMD => "JMD",
-                Currency.BBD => "BBD",
-                Currency.BZD => "BZD",
-                Currency.XCD => "XCD",
-                Currency.TTD => "TTD",
-                Currency.GYD => "GYD",
-                Currency.SRD => "SRD",
-                Currency.FKP => "FKP",
-                Currency.NZD => "NZD",
-                Currency.FJD => "FJD",
-                Currency.PGK => "PGK",
-                Currency.SBD => "SBD",
-                Currency.VUV => "VUV",
-                Currency.WST => "WST",
-                Currency.TOP => "TOP",
-                Currency.XPF => "XPF",
-                _ => "USD"
+                Currency.USD => "$",        // US Dollar
+                Currency.EUR => "€",        // Euro
+                Currency.GBP => "£",        // British Pound
+                Currency.JPY => "¥",        // Japanese Yen
+                Currency.LBP => "L.L",      // Lebanese Pound
+                Currency.CAD => "CA$",      // Canadian Dollar
+                Currency.AUD => "A$",       // Australian Dollar
+                Currency.CHF => "Fr",       // Swiss Franc
+                Currency.CNY => "¥",        // Chinese Yuan
+                Currency.SEK => "kr",       // Swedish Krona
+                Currency.NOK => "kr",       // Norwegian Krone
+                Currency.DKK => "kr",       // Danish Krone
+                Currency.PLN => "zł",       // Polish Zloty
+                Currency.CZK => "Kč",       // Czech Koruna
+                Currency.HUF => "Ft",       // Hungarian Forint
+                Currency.RON => "lei",      // Romanian Leu
+                Currency.BGN => "лв",       // Bulgarian Lev
+                Currency.HRK => "kn",       // Croatian Kuna
+                Currency.RSD => "дин",      // Serbian Dinar
+                Currency.MKD => "ден",      // Macedonian Denar
+                Currency.BAM => "KM",       // Bosnia and Herzegovina Convertible Mark
+                Currency.ALL => "L",        // Albanian Lek
+                Currency.MNT => "₮",        // Mongolian Tugrik
+                Currency.KRW => "₩",        // South Korean Won
+                Currency.SGD => "S$",       // Singapore Dollar
+                Currency.HKD => "HK$",      // Hong Kong Dollar
+                Currency.TWD => "NT$",      // New Taiwan Dollar
+                Currency.THB => "฿",        // Thai Baht
+                Currency.MYR => "RM",       // Malaysian Ringgit
+                Currency.IDR => "Rp",       // Indonesian Rupiah
+                Currency.PHP => "₱",        // Philippine Peso
+                Currency.VND => "₫",        // Vietnamese Dong
+                Currency.INR => "₹",        // Indian Rupee
+                Currency.PKR => "₨",        // Pakistani Rupee
+                Currency.BDT => "৳",        // Bangladeshi Taka
+                Currency.LKR => "Rs",       // Sri Lankan Rupee
+                Currency.NPR => "₨",        // Nepalese Rupee
+                Currency.AFN => "؋",        // Afghan Afghani
+                Currency.IRR => "﷼",        // Iranian Rial
+                Currency.IQD => "ع.د",      // Iraqi Dinar
+                Currency.JOD => "د.ا",      // Jordanian Dinar
+                Currency.KWD => "د.ك",      // Kuwaiti Dinar
+                Currency.BHD => ".د.ب",     // Bahraini Dinar
+                Currency.QAR => "ر.ق",      // Qatari Rial
+                Currency.AED => "د.إ",      // UAE Dirham
+                Currency.OMR => "ر.ع.",     // Omani Rial
+                Currency.SAR => "ر.س",      // Saudi Riyal
+                Currency.YER => "﷼",        // Yemeni Rial
+                Currency.EGP => "ج.م",      // Egyptian Pound
+                Currency.MAD => "د.م.",     // Moroccan Dirham
+                Currency.TND => "د.ت",      // Tunisian Dinar
+                Currency.DZD => "د.ج",      // Algerian Dinar
+                Currency.LYD => "ل.د",      // Libyan Dinar
+                Currency.SDG => "ج.س.",     // Sudanese Pound
+                Currency.ETB => "Br",       // Ethiopian Birr
+                Currency.KES => "KSh",      // Kenyan Shilling
+                Currency.UGX => "USh",      // Ugandan Shilling
+                Currency.TZS => "TSh",      // Tanzanian Shilling
+                Currency.ZAR => "R",        // South African Rand
+                Currency.NGN => "₦",        // Nigerian Naira
+                Currency.GHS => "GH₵",      // Ghanaian Cedi
+                Currency.XOF => "CFA",      // West African CFA Franc
+                Currency.XAF => "FCFA",     // Central African CFA Franc
+                Currency.BRL => "R$",       // Brazilian Real
+                Currency.ARS => "$",        // Argentine Peso
+                Currency.CLP => "$",        // Chilean Peso
+                Currency.COP => "$",        // Colombian Peso
+                Currency.MXN => "$",        // Mexican Peso
+                Currency.PEN => "S/.",      // Peruvian Sol
+                Currency.UYU => "$U",       // Uruguayan Peso
+                Currency.VES => "Bs.S",     // Venezuelan Bolívar Soberano
+                Currency.RUB => "₽",        // Russian Ruble
+                Currency.UAH => "₴",        // Ukrainian Hryvnia
+                Currency.BYN => "Br",       // Belarusian Ruble
+                Currency.KZT => "₸",        // Kazakhstani Tenge
+                Currency.UZS => "сум",      // Uzbekistani Som
+                Currency.KGS => "с",        // Kyrgyzstani Som
+                Currency.TJS => "SM",       // Tajikistani Somoni
+                Currency.TMT => "m",        // Turkmenistani Manat
+                Currency.AZN => "₼",        // Azerbaijani Manat
+                Currency.AMD => "֏",        // Armenian Dram
+                Currency.GEL => "₾",        // Georgian Lari
+                Currency.TRY => "₺",        // Turkish Lira
+                Currency.JMD => "J$",       // Jamaican Dollar
+                Currency.BBD => "Bds$",     // Barbadian Dollar
+                Currency.BZD => "BZ$",      // Belize Dollar
+                Currency.XCD => "EC$",      // East Caribbean Dollar
+                Currency.TTD => "TT$",      // Trinidad and Tobago Dollar
+                Currency.GYD => "G$",       // Guyanese Dollar
+                Currency.SRD => "$",        // Surinamese Dollar
+                Currency.FKP => "£",        // Falkland Islands Pound
+                Currency.NZD => "NZ$",      // New Zealand Dollar
+                Currency.FJD => "FJ$",      // Fijian Dollar
+                Currency.PGK => "K",        // Papua New Guinean Kina
+                Currency.SBD => "SI$",      // Solomon Islands Dollar
+                Currency.VUV => "VT",       // Vanuatu Vatu
+                Currency.WST => "WS$",      // Samoan Tala
+                Currency.TOP => "T$",       // Tongan Pa'anga
+                Currency.XPF => "₣",        // CFP Franc
+                _ => "$"                    // Default to USD symbol
             };
         }
 
@@ -157,7 +157,6 @@ namespace IMS_Mobile.Converters
         {
             if (value is string stringValue)
             {
-                // Extract numeric value from currency string
                 var parts = stringValue.Split(' ');
                 if (parts.Length > 0 && double.TryParse(parts[0], out double result))
                 {
