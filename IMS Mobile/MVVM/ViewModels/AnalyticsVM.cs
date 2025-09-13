1051cd9011336b0285bc289494af2a77d97d93c2
Bhbored <bhbored2022@gmail.com>
2025-09-13 18:06:51 +0300

    done with the first chart

--- IMS Mobile/MVVM/ViewModels/AnalyticsVM.cs
+++ IMS Mobile/MVVM/ViewModels/AnalyticsVM.cs
@@ -9,6 +9,8 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Input;
 using Transaction = IMS_Mobile.MVVM.Models.Transaction;
+using Product = IMS_Mobile.MVVM.Models.Product;
+using TransactionProductItem = IMS_Mobile.MVVM.Models.TransactionProductItem;
 
 namespace IMS_Mobile.MVVM.ViewModels
 {
@@ -29,7 +31,7 @@ namespace IMS_Mobile.MVVM.ViewModels
         private ObservableCollection<Transaction> cashFlowTransactions = new ObservableCollection<Transaction>();
 
         public int PageIndex { get; set; } = 1;
- 
+
 
         #region Pagination Logic
 
@@ -125,6 +127,102 @@ namespace IMS_Mobile.MVVM.ViewModels
                 .ToList();
             ApplyPagination();
         }
+
+        #region Pyramid Chart Data
+
+        public ObservableCollection<Product> Products { get; set; } = new ObservableCollection<Product>();
+        public ObservableCollection<TransactionProductItem> TransactionItems { get; set; } = new ObservableCollection<TransactionProductItem>();
+        public ObservableCollection<object> TopSellingProducts { get; set; } = new ObservableCollection<object>();
+
+        private void LoadMockProducts()
+        {
+            var random = new Random();
+            var productNames = new[]
+            {
+                "iPhone 15 Pro", "Samsung Galaxy S24", "MacBook Pro M3", "Dell XPS 13", "iPad Air",
+                "Surface Pro 9", "AirPods Pro", "Sony WH-1000XM5", "Nintendo Switch", "PlayStation 5",
+                "Xbox Series X", "Apple Watch Ultra", "Samsung Galaxy Watch", "Fitbit Versa", "Garmin Fenix",
+                "Canon EOS R5", "Sony A7 IV", "Nikon Z9", "GoPro Hero 12", "DJI Mini 4 Pro"
+            };
+
+            Products.Clear();
+
+            for (int i = 0; i < 20; i++)
+            {
+                var product = new Product
+                {
+                    Id = i + 1,
+                    Name = productNames[i],
+                    Price = random.Next(50, 2000),
+                    Cost = random.Next(30, 1500),
+                    stock = random.Next(0, 100),
+                    CreatedDate = DateTime.Now.AddDays(-random.Next(0, 365))
+                };
+
+                Products.Add(product);
+            }
+        }
+
+        private void LoadMockTransactionItems()
+        {
+            var random = new Random();
+            TransactionItems.Clear();
+
+            for (int i = 0; i < 100; i++)
+            {
+                var productId = random.Next(1, 21);
+                var quantity = random.Next(1, 10);
+                var product = Products.FirstOrDefault(p => p.Id == productId);
+
+                var transactionItem = new TransactionProductItem
+                {
+                    Id = i + 1,
+                    Name = product?.Name ?? $"Product {productId}",
+                    Price = product?.Price ?? random.Next(50, 2000),
+                    Cost = product?.Cost ?? random.Next(30, 1500),
+                    Quantity = quantity,
+                    TransactionId = random.Next(1, 50)
+                };
+
+                TransactionItems.Add(transactionItem);
+            }
+        }
+
+        private void CalculateTopSellingProducts()
+        {
+            var topProducts = TransactionItems
+                .GroupBy(item => item.Name)
+                .Select(group => new
+                {
+                    Name = group.Key,
+                    TotalQuantity = group.Sum(item => item.Quantity),
+                    TotalRevenue = group.Sum(item => item.TotalPrice)
+                })
+                .OrderByDescending(x => x.TotalQuantity)
+                .Take(8)
+                .ToList();
+
+            TopSellingProducts.Clear();
+
+            foreach (var product in topProducts)
+            {
+                TopSellingProducts.Add(new
+                {
+                    Name = product.Name,
+                    Value = product.TotalQuantity,
+                    Revenue = product.TotalRevenue
+                });
+            }
+        }
+
+        public void LoadPyramidChartData()
+        {
+            LoadMockProducts();
+            LoadMockTransactionItems();
+            CalculateTopSellingProducts();
+        }
+
+        #endregion
 
 
 
--- IMS Mobile/MVVM/Views/Analytics.xaml.cs
+++ IMS Mobile/MVVM/Views/Analytics.xaml.cs
@@ -5,23 +5,24 @@ namespace IMS_Mobile.MVVM.Views;
 
 public partial class Analytics : ContentPage
 {
-    public Analytics(AnalyticsVM viewModel)
-    {
-        InitializeComponent();
-        BindingContext = viewModel;
-    }
-    protected override void OnAppearing()
-    {
-        base.OnAppearing();
-        if (BindingContext is AnalyticsVM viewModel)
-        {
-            _ = Task.Run(async () =>
-            {
-                await MainThread.InvokeOnMainThreadAsync(async () =>
-                {
-                    await viewModel.RefreshDataAsync();
-                });
-            });
-        }
-    }
+	public Analytics(AnalyticsVM viewModel)
+	{
+		InitializeComponent();
+		BindingContext = viewModel;
+	}
+	protected override void OnAppearing()
+	{
+		base.OnAppearing();
+		if (BindingContext is AnalyticsVM viewModel)
+		{
+			_ = Task.Run(async () =>
+			{
+				await MainThread.InvokeOnMainThreadAsync(async () =>
+				{
+					await viewModel.RefreshDataAsync();
+					viewModel.LoadPyramidChartData();
+				});
+			});
+		}
+	}
 }
